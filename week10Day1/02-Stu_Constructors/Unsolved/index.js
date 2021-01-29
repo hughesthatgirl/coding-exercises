5fce2f9587fd8b4239230b5ded4522c3e4482f3c
JoannaChandler <jhazimih@gmail.com>
2021-01-28 19:08:46 -0500

    Solving the constructor exercise

--- week10Day1/02-Stu_Constructors/Unsolved/index.js
+++ week10Day1/02-Stu_Constructors/Unsolved/index.js
@@ -1,7 +1,16 @@
 // TODO: Create a constructor function called 'Developer' that takes in 'name' and 'tech'
 
-// TODO: Include a method called 'introduction()' that introduces the Developer with their name and favorite tech
+function Developer(name, tech){
+    this.name = name;
+    this.tech = tech;
+    this.introduction = function(){
+        console.log(`Hi ${name}! You Love ${tech}!`)
+    }
+}
 
 // TODO: Create a new object using the 'Developer' constructor
+const dev = new Developer('Joanna', 'Javascript');
 
 // TODO: Call the 'introduction()' method on the new object
+
+dev.introduction();
