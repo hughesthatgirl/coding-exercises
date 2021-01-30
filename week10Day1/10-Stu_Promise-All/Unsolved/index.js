64828592956cf486c7972a2b603bda57a314a857
JoannaChandler <jhazimih@gmail.com>
2021-01-30 10:01:37 -0500

    Adding in files for week10 day 3

--- week10Day1/10-Stu_Promise-All/Unsolved/index.js
+++ week10Day1/10-Stu_Promise-All/Unsolved/index.js
@@ -5,6 +5,11 @@ const callAPI = (duration) =>
   new Promise((resolve, reject) => {
     setTimeout(() => {
       // TODO: If the duration is longer than maxDuration, reject() the promise
+      if (duration > maxDuration){
+        reject ('Error');
+      } else {
+        resolve (duration)
+      }
       // TODO: Otherwise resolve() the promise
     }, duration);
   });
@@ -16,3 +21,12 @@ apiCallDurations.map((duration) => promises.push(callAPI(duration)));
 console.log('Promises array before the timeouts have finished: ', promises);
 
 // TODO: Use Promise.all() to capture when the array of promises has been resolved or if any of them were rejected
+Promise.all(promises)
+  .then(function(values){
+    values.forEach(function(value){
+      console.log(`Response received: ${value} ms`);
+    })
+  })
+  .catch(function(err){
+    console.log(err);
+  });
