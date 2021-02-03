830161864bfd4059dde16fc9a906ddb85aef45c5
JoannaChandler <jhazimih@gmail.com>
2021-02-02 19:41:28 -0500

    Activity 2

--- week10Day2/14-Stu_Pass-Tests/Unsolved/algo.js
+++ week10Day2/14-Stu_Pass-Tests/Unsolved/algo.js
@@ -1,15 +1,23 @@
 function Algo() {}
 
 Algo.prototype.reverse = function(str) {
-  // TODO: Your code here
+  return str.split('').reverse().join('');
 };
 
 Algo.prototype.isPalindrome = function(str) {
-  // TODO: Your code here
+  
 };
 
 Algo.prototype.capitalize = function(str) {
-  // TODO: Your code here 
+  const words = str.split(' ');
+
+  for (let i = 0; i < words.length; i++){
+    words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+  }
+
+  words.join(' ');
+
+  return words;
 };
 
 module.exports = Algo;
